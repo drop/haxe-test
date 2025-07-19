a4ff126252160544f9445276920e2f280da90644
drop <drop@mail.com>
2025-07-19 21:46:35 +0300

    The class inheritance and the interfaces implementing

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-## Building to javascript
+# Project description
+
+Here I am learning the Haxe language and trying out its libraries
+
+## Build to javascript
 
 ```bash
 haxe build.hxml
--- src/app/Main.hx
+++ src/app/Main.hx
@@ -1,21 +1,47 @@
 package app;
 
-class Main {
-  private var testVar:Int = 0xfff;
+interface IMain {
+  private function printTestVar():Void;
+}
+
+interface ITestVarGetter {
+  private var testVar:Int;
+  public function getTestVar():Int;
+}
+
+class Main
+  extends MainParentClass
+  implements IMain implements ITestVarGetter {
+
+  private var testVar:Int = 0x002;
 
   static function main():Void {
+    trace('--------------------------');
     trace('Running static main method');
 
+    MainParentClass.traceFromParent('Parent #1');
+
     new Main('Instance #1');
+    trace('--------------------------');
   }
 
   function new(clName: String) {
-    trace('Main class instance creation: ', clName);
+    trace('Main class instance creation: ' + clName);
 
     this.printTestVar();
   }
 
   private function printTestVar():Void {
-    trace('Private VAR = ', this.testVar);
+    trace('Private testVar = ' + this.testVar);
   }
+
+  public function getTestVar():Int {
+    return this.testVar; 
+  }
+}
+
+class MainParentClass {
+  static function traceFromParent(outStr:String) {
+    trace('Trace from MainParentClass: ' + outStr);
+  } 
 }
